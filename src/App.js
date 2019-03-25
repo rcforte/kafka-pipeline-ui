5997c0b942663a59bdad6940c7b649b1b2d4b626
Rafael Forte <rcforte@gmail.com>
2019-03-25 07:23:55 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {LineChart, Line} from 'recharts';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {prices:[]}
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      if(true) {
+        console.log('sending request to the server...');
+        fetch('http://localhost:8080/api/v1/prices/')
+          .then(res => res.json())
+          .then(prices => this.setState({prices}));
+      } else {
+        // for debugging
+        console.log('rendering')
+        var data = []
+        for(var i=0; i<10; i++)
+          data.push({sym:'SPX Index', price:Math.random()});
+        this.setState({prices:data});
+      }
+    }, 3000);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <LineChart width={400} height={400} data={this.state.prices}>
+        <Line type="monotone" dataKey="price" stroke="#8884d8"/>
+      </LineChart>
     );
   }
 }
