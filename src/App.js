288d9c96c933acae0c8a8eef82616da1bd398057
Rafael Forte <rcforte@gmail.com>
2019-03-25 18:00:15 -0400

    Using server side events

--- src/App.js
+++ src/App.js
@@ -6,32 +6,45 @@ import {LineChart, Line} from 'recharts';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {prices:[]}
+    this.eventSource = new EventSource('//localhost:8080/api/v1/prices/sse');
+    this.updatePrice = this.updatePrice.bind(this);
+    this.makeRestCall = this.makeRestCall.bind(this);
+    this.state = {
+      prices: []
+    };
   }
 
   componentDidMount() {
+    this.eventSource.onmessage = e => this.updatePrice(JSON.parse(e.data));
+  }
+
+  makeRestCall(){
     setInterval(() => {
-      if(true) {
-        console.log('sending request to the server...');
-        fetch('http://localhost:8080/api/v1/prices/')
-          .then(res => res.json())
-          .then(prices => this.setState({prices}));
-      } else {
-        // for debugging
-        console.log('rendering')
-        var data = []
-        for(var i=0; i<10; i++)
-          data.push({sym:'SPX Index', price:Math.random()});
-        this.setState({prices:data});
-      }
+      console.log('sending request to the server...');
+      fetch('http://localhost:8080/api/v1/prices/')
+        .then(res => res.json())
+        .then(prices => this.setState({prices}));
     }, 3000);
+  }
+
+  updatePrice(newPrice) {
+    let oldPrices = this.state.prices;
+    let newPrices = []
+    for(var i=0; i<oldPrices.length; i++)
+      newPrices.push(oldPrices[i]);
+    newPrices.push(newPrice);
+    this.setState({prices: newPrices});
   }
 
   render() {
     return (
-      <LineChart width={400} height={400} data={this.state.prices}>
-        <Line type="monotone" dataKey="price" stroke="#8884d8"/>
-      </LineChart>
+      <div>
+        <div>
+          <LineChart width={400} height={400} data={this.state.prices}>
+            <Line type="monotone" dataKey="price" stroke="#8884d8"/>
+          </LineChart>
+        </div>
+      </div>
     );
   }
 }
